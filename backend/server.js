0f8a8215cac6328c3bd66d04a045fdde0ab88b5c
Niraj Kumar <kumarniraj8607@gmail.com>
2026-05-05 17:00:31 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ const PORT=process.env.PORT || 3000 ;
 //middleware
 app.use(express.json());
 app.use(cors({
-    origin : "http://localhost:5173",
+    // origin : "http://localhost:5173",
+    origin : "*",
     credentials : true
 }))
 
